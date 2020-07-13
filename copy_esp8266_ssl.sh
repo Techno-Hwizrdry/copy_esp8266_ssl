1ff5819ca9bfb5a3bd4df29e6744729301d55d67
Alexan Mardigian <alex.mardigian@gmail.com>
2020-07-13 14:02:48 -0400

    Modified to take command line parameters
    Also, provides an option to either keep or delete the backed up firmware image after exporting the SSL key to a specified file.

--- copy_esp8266_ssl.sh
+++ copy_esp8266_ssl.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
-COPIED_IMG="backup.img"
-SSL_OUT="ssl_cert_output.txt"
+SSL_OUT=$1
+COPIED_IMG=$2
+REMOVE_IMG=false
+PORT=$3
 
-{ time esptool.py --port /dev/ttyUSB0 --baud 115200 read_flash 0x00000 0x400000 $COPIED_IMG > /dev/null; } 2>&1 | grep real
+if [ -z "$SSL_OUT" ]
+  then
+    echo ""
+    echo "Basic usage: $0 OUTPUT-FILE-NAME"
+    echo ""
+    exit 0
+fi
+
+if [ -z "$COPIED_IMG" ]
+  then
+    COPIED_IMG="backup.img"
+    REMOVE_IMG=true
+fi
+
+if [ -z "$PORT" ]
+  then
+    PORT=/dev/ttyUSB0
+fi
+
+{ time esptool.py --port $PORT --baud 115200 read_flash 0x00000 0x400000 $COPIED_IMG > /dev/null; } 2>&1 | grep real
 awk '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/' $COPIED_IMG > $SSL_OUT
+
+if [ "$REMOVE_IMG" == true ]
+  then
+    rm $COPIED_IMG
+fi
